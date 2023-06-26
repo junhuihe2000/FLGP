955616bd8cb5301f52d8564ead081f5945ad8e1d
He Junhui <hejunhui.math@gmail.com>
2023-06-26 15:24:28 +0800

    append document for subsample

--- R/subsample.R
+++ R/subsample.R
@@ -1,11 +1,17 @@
-# sub-sampling
-
-# para:
-#     X: (n, d) matrix, original sample, each row indicates one point in R^d
-#     s: integer, the number of the sub-sample
-#     methods = c("kmeans", "random"), how to sub-sample
-# return value:
-#     U: (s, d) matrix, sub-sample, each row indicates one "new" point in R^d
+#' Subsample in the domain
+#'
+#' @param X Original sample, a (n, d) matrix, each row indicates one point in R^d.
+#' @param s An integer indicating the number of the subsampling.
+#' @param method How to subsample, including kmeans and random selection, the
+#' defaulting subsampling method is kmeans.
+#'
+#' @return U Subsampling, a (s, d) matrix, each row indicates one point in R^d.
+#' @export
+#'
+#' @examples
+#' X <- matrix(rnorm(100*3), nrow=100, ncol=3)
+#' s <- 10
+#' U = subsample(X, s, method = "kmeans")
 subsample <- function(X, s, method = "kmeans") {
   if(method == "kmeans") {
     U = kmeans(X, s, iter.max = 20, nstart = 10)$centers
