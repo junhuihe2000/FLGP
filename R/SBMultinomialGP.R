c5773a255349c97779eb57ef888ed71061566dad
He Junhui <hejunhui.math@gmail.com>
2023-07-08 16:52:40 +0800

    append SBMultinomialGP

---
+++ R/SBMultinomialGP.R
@@ -0,0 +1,24 @@
+#' Construct stick breaking multinomial Gaussian process models
+#'
+#' @param C A numeric matrix with dim(m, m), the covariance matrix of training samples,
+#' where `C[i,j]` indicates the covariance between `X_i` and `X_j`.
+#' @param Y A numeric matrix with dim(m,J), each row is a one hot vector indicating
+#' the label.
+#'
+#' @return An instance of the class `SBMultinomialGP`.
+#' @export
+#'
+#' @examples
+#' A <- matrix(rnorm(3*3),3,3)
+#' C <- A%*%t(A)
+#' Y <- diag(rep(1,3))
+#' SBMultinomialGP(C,Y)
+SBMultinomialGP <- function(C, Y) {
+  stopifnot(methods::is(C, "matrix")||methods::is(C, "Matrix"),
+            methods::is(Y,  "matrix")||methods::is(C, "Matrix"))
+  stopifnot(nrow(C)==ncol(C), nrow(C)==nrow(Y))
+  J = ncol(Y); m = nrow(Y)
+  model = list(class=J, m=m, covariance=C, label=Y)
+  class(model) = "SBMultinomialGP"
+  return(model)
+}
