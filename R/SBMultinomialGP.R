4ce9b38cd47b60d38a8ae253ddec41dc209b4fae
He Junhui <hejunhui.math@gmail.com>
2023-07-08 17:56:33 +0800

    append initializeSBMGP

--- R/SBMultinomialGP.R
+++ R/SBMultinomialGP.R
@@ -11,14 +11,29 @@
 #' @examples
 #' A <- matrix(rnorm(3*3),3,3)
 #' C <- A%*%t(A)
-#' Y <- diag(rep(1,3))
+#' Y <- matrix(sample.int(3*3 ,replace=TRUE),3,3)
 #' SBMultinomialGP(C,Y)
 SBMultinomialGP <- function(C, Y) {
   stopifnot(methods::is(C, "matrix")||methods::is(C, "Matrix"),
             methods::is(Y,  "matrix")||methods::is(C, "Matrix"))
   stopifnot(nrow(C)==ncol(C), nrow(C)==nrow(Y))
   J = ncol(Y); m = nrow(Y)
-  model = list(class=J, m=m, covariance=C, label=Y)
+  N = N_vec(Y); kappa = kappa_vec(Y, N)
+
+  # initialize the auxiliary variable
+  omega = matrix(1, M, J-1)
+
+  # initialize a "sample" from f
+  f = matrix(0, M, J-1)
+
+  model = list(J=J,
+               m=m,
+               C=C,
+               Y=Y,
+               N=N,
+               kappa=kappa,
+               omega=omega,
+               f=f)
   class(model) = "SBMultinomialGP"
   return(model)
 }
